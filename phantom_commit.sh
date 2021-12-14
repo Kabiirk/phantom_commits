2ef306af8b89042d10db24a9cf3e6b70f0ff9b1e
Kabiir <kabiirk@gmail.com>
2021-12-15 00:24:30 +0530

    Added code to commit in past by adding a base commit

--- phantom_commit.sh
+++ phantom_commit.sh
@@ -4,6 +4,15 @@ read -p "Enter Commit date : " date
 echo $mssg
 echo $date
 
+# git checkout master
+# git checkout --orphan new-root
+# git rm -rf .
+# git add path/to/file
+# GIT_AUTHOR_DATE='$1' GIT_COMITTER_DATE='$1' git commit
+# git checkout -
+# git rebase --root --onto new-root
+# git branch -d new-root
+
 # Ref :
 # https://stackoverflow.com/questions/3895453/how-do-i-make-a-git-commit-in-the-past
 # https://stackoverflow.com/questions/23609991/git-github-commit-at-past-date
